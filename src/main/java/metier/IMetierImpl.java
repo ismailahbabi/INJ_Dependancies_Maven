458cca7c77b550bd7049a2708f9dbdec0004cf58
ismailahbabi <ismail.lahbabi17@gmail.com>
2022-03-16 00:50:27 +0100

    Tp injection des dependances

--- src/main/java/metier/IMetierImpl.java
+++ src/main/java/metier/IMetierImpl.java
@@ -2,8 +2,14 @@ package metier;
 
 
 import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 
 public class IMetierImpl implements IMetier {
+
+    @Autowired
     //couplage faible
     private IDao dao;
     @Override
